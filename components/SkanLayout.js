5dcd89e8f623f40ee88191d775033ca96771bb35
skandog <muumrar@gmail.com>
2022-07-12 18:21:42 +0100

    having trouble controlling the nav bar that I want. Leaving me wondering whether I want to switch libraries, or just create it myself? hmm

--- components/SkanLayout.js
+++ components/SkanLayout.js
@@ -2,20 +2,20 @@ import {Layout, Menu} from 'antd'
 import React, { Children } from 'react'
 const {Header, Content, Footer} = Layout
 import styles from '../styles/Home.module.css'
+//import '../styles/SkanLayout.css'
+
 
 const SkanLayout = ({children}) => {
    
-    
+    const items = [{label: 'item 1', key: 'item-1' },  { label: 'item 2', key: 'item-2' }]
     
     return (
         <Layout className="layout" >
-        <Header className={styles.header}>
-            <div className='logo' />
-            <Menu
-        theme="dark"
-        mode="horizontal"
-     
-      />
+        <Header style={{background: "#001529"}} className={styles.header}>
+            <div className={styles.logo} />
+            <Menu items={items} mode="horizontal" theme="dark" />
+
+            
         </Header>
         {children}
         <Footer className={styles.footer}> Skandog designs ©2022 Created by Skandy with help from School of Code</Footer>
---
+++ pages/about.js
@@ -0,0 +1,5 @@
+const About = () => {
+    return ( <p>Hey I'm skandy and i am here to code</p> );
+}
+ 
+export default About;
